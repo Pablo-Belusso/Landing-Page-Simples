74e983916031b3d5f1a3f231a972ae0e6758d9e4
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-15 17:06:29 -0300

    Commit 05

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import NavBar from "../components/NavBar";
 import PokemonCard from "../components/PokemonCard";
+import axios from "axios";
 
 export const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons();
+  }, []);
+  const getPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .then((res) => setPokemons(res.data.results));
+    //.cath((err) => console.log(err));
+  };
+
   return (
     <div>
       <NavBar />
 
       <Container maxWidth="xg">
         <Grid container>
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
-
-          <Grid item xs={3}>
-            <PokemonCard />
-          </Grid>
+          {pokemons.map((pokemon) => (
+            <Grid item xs={3}>
+              <PokemonCard />
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </div>
