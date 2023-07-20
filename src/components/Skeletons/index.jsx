951cb2c0481e98c4f958bbec162d081f84a98ce8
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-07-20 13:38:22 -0300

    Commit 16

--- src/components/Skeletons/index.jsx
+++ src/components/Skeletons/index.jsx
@@ -1,14 +1,39 @@
-import { Skeleton } from "@mui/material";
+import { Container, Skeleton } from "@mui/material";
 import React from "react";
 
 export const Skeletons = () => {
   return (
-    <>
-      <Skeleton variant="rounded" width="100%" height={100} />;
-      <Skeleton variant="rounded" width="100%" height={100} />;
-      <Skeleton variant="rounded" width="100%" height={100} />;
-      <Skeleton variant="rounded" width="100%" height={100} />;
-      <Skeleton variant="rounded" width="100%" height={100} />;
-    </>
+    <Container maxWidth="xxl">
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={100}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={100}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={100}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={100}
+        sx={{ marginBottom: "1em" }}
+      />
+      <Skeleton
+        variant="rounded"
+        width="100%"
+        height={100}
+        sx={{ marginBottom: "1em" }}
+      />
+    </Container>
   );
 };
